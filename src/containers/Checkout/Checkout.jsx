4c1520f739b44eaee3ceca58ae93235b3805dfef
Jules Douglas <jules.douglas@hotmail.com>
2020-05-13 17:38:04 -0500

    minor update redux

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -5,10 +5,10 @@ import ContactData from './ContactData/ContactData';
 import {connect} from 'react-redux';
 
 class Checkout extends React.Component {
-  state = {
-    ingredients: null,
-    price: 0
-  };
+  // state = {
+  //   ingredients: null,
+  //   price: 0
+  // };
 
   // componentWillMount() {
   //   const query = new URLSearchParams(this.props.location.search);
@@ -37,22 +37,21 @@ class Checkout extends React.Component {
     return (
       <div>
         <CheckoutSummary
-          ingredients={this.state.ingredients}
+          ingredients={this.props.ings}
           checkoutCancel={this.checkoutCancelHandler}
           checkoutContinue={this.checkoutContinueHandler}/>
         <Route
           path={this.props.match.path + '/contact-data'}
           //render={(props)=>(<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}
-          component={ContactData}/>
+          component = {ContactData}/>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return{
-    ings: state.ingredients,
-
+  return {
+    ings: state.ingredients
   }
 };
 
