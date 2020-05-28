b895a030373a066fc1345727e82b7c29d9aa79ee
Jules Douglas <jules.douglas@hotmail.com>
2020-05-27 22:55:18 -0500

    created a login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 
 
@@ -16,7 +17,8 @@ export default class App extends React.Component {
           <Switch>
             <Route path = "/checkout" component = {Checkout}/>
             <Route path = "/orders" component = {Orders} />
-            <Route path="/" exacct component={BurgerBuilder}/>
+            < Route path = "/auth"  component = {Auth}/>
+            <Route path = "/" exacct component={BurgerBuilder}/>
           </Switch>
         </Layout>
       </div>
